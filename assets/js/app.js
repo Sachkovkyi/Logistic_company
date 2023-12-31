4f7ec5067a361b235c3c39ce092a752a4a644532
Yuri Sachkovkyi <kach488@gmail.com>
2023-12-31 17:25:22 +0200

    Second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,4 +36,84 @@
         console.log(scrollElPos)
     })
 
+    // ScrollSpy
+
+
+
 });
+
+$(document).ready(function(){
+
+    var sectionIds = $('a.nav__link');
+  
+      $(document).scroll(function(){
+          sectionIds.each(function(){
+  
+              var container = $(this).attr('href');
+              var containerOffset = $(container).offset().top;
+              var containerHeight = $(container).outerHeight();
+              var containerBottom = containerOffset + containerHeight;
+              var scrollPosition = $(document).scrollTop();
+      
+              if(scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20){
+                  $(this).addClass('active');
+              } else{
+                  $(this).removeClass('active');
+              }
+      
+      
+          });
+      });
+
+
+    //   Modal
+
+    $('[data-modal]').on('click', function(){
+        event.preventDefault();
+
+        let modal = $(this).data('modal')
+        console.log(modal);
+
+        $('body').addClass('no-scroll');
+
+        $(modal).addClass('show')
+    })
+
+    $('[data-modal-close]').on('click', function() {
+        event.preventDefault();
+
+        let modal = $(this).parents('.modal');
+        
+        $('body').removeClass('no-scroll');
+        modal.removeClass('show');
+
+    })
+
+    $('modal').on('click', function(){
+
+        $('body').removeClass('no-scroll');
+        $('modal').removeClass('show');
+
+    });
+
+    $('modal__content').on('click', function(){
+        event.stopPropagation();
+
+    });
+
+
+    
+     let navToogle = $('#navToogle');
+     let nav = $('#nav');
+
+     navToogle.on('click', function(){
+            event.preventDefault();
+
+            nav.toogleClass('show');
+
+     });
+     
+  
+  
+  });
+
